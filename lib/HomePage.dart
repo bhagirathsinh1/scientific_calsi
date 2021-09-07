b244f0b2af17c33bd9c1a78fc3d9ff7e02a64b6f
bhagirathsinh dodiya <bhagidodiya56@gmail.com>
2021-09-07 16:21:12 +0530

    okojo

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var userQuestion = '';
   var userAnswer = '';
-  var viewHistory = 'View History okokokoook';
+  var viewHistory = 'View History okooook';
   var ob = 0;
   var cb = 0;
   var zero = 0;
