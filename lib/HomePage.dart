bee5eb0386c593074c325af2e7ea99571ea5f870
bhagirathsinh dodiya <bhagidodiya56@gmail.com>
2021-09-07 16:42:11 +0530

    history changed

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var userQuestion = '';
   var userAnswer = '';
-  var viewHistory = 'View History';
+  var viewHistory = 'View History changed';
   var ob = 0;
   var cb = 0;
   var zero = 0;
