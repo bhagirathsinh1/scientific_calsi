6890cd67e1a682fb8c7ede39c9016418868d5ff2
bhagirathsinh dodiya <bhagidodiya56@gmail.com>
2021-09-07 16:45:48 +0530

    second commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var userQuestion = '';
   var userAnswer = '';
-  var viewHistory = 'View History changedc';
+  var viewHistory = 'View History change dc';
   var ob = 0;
   var cb = 0;
   var zero = 0;
